56b34e76445b48d8ea073c2517a3d320b813f636
Aiden Cline <aidenpcline@gmail.com>
2024-09-21 15:14:09 -0500

    get pages router working <- why didn't app router need the fetch adapter for trpc???

--- cli/template/extras/src/pages/api/auth/[...nextauth].ts
+++ cli/template/extras/src/pages/api/auth/[...nextauth].ts
@@ -2,4 +2,8 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default NextAuth(authOptions);
--- cli/template/extras/src/pages/api/trpc/[trpc].ts
+++ cli/template/extras/src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,27 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
 
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
-});
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    router: appRouter,
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+    req,
+    createContext: createTRPCContext,
+  });
+}
